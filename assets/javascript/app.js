23bd3700d1f3c74fc14fd5132c87c3cb95337208
Zach Merel <zachmerel@gmail.com>
2018-11-07 18:15:18 -0600

    gifs load with button click, click toggle animation, double click stores to localstorage and places gif into favorites seciton.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,42 @@
 var shows = ["The Simpsons", "Malcom in the Middle", "Breaking Bad", "Shameless"];
 
 // displayMovieInfo function re-renders the HTML to display the appropriate content
+
+function singleClick (){
+$("img.gif").on("click", function () {
+    console.log("single click working")
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("tvShow-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("tvShow-still"));
+        $(this).attr("data-state", "still");
+    }
+});
+}
+function doubleClick(){
+$(".doubleclick").dblclick(function () {
+    // event.preventDefault();
+    console.log("double click working")
+    var favoriteGif = localStorage.getItem("favoriteGifs")
+    if (favoriteGif == null){
+        favoriteGif = "";
+    }
+    // double click takes everything inside div of what was double click stores it to localstorage
+    var newFavoriteGif = $(this)[0].innerHTML
+   favoriteGif  += newFavoriteGif;
+   localStorage.setItem("favoriteGifs", favoriteGif);
+    $("#favorite-gifs").append(`<div>${newFavoriteGif}</div>`);
+    console.log(newFavoriteGif);
+    singleClick ();
+    
+});
+}
 function displayTVInfo() {
     var tvShow = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -18,7 +54,7 @@ function displayTVInfo() {
             // Looping over every result item
             for (var i = 0; i < results.length; i++) {
                 // Creating a div for the gif with id to place double click on
-                var gifDiv = $(`<div id=doubleclick>`);
+                var gifDiv = $("<div class='doubleclick'>");
 
                 // Storing the result item's rating
                 var rating = results[i].rating;
@@ -39,20 +75,7 @@ function displayTVInfo() {
                 tvShowImage.attr("tvShow-animate", results[i].images.original.url);
                 // gives image a class of gif
                 tvShowImage.addClass("gif");
-
-
-                
-                
-                $("#doubleclick").dblclick(function () {
-                  //double click takes everything inside div of what was double click stores it to localstorage
-                     localStorage.setItem("favorite gifs",$(this)[0].innerHTML);
-                     var favoriteGifs = localStorage.getItem("favorite gifs")
-                    $("#favorite-gifs").html(`<div>${favoriteGifs}</div>`);
-                    console.log($(this)[0].innerHTML);
-                });
-
-
-
+               
 
                 // Appending the paragraph and tvShowImage we created to the "gifDiv" div we created
                 gifDiv.append(tvShowImage);
@@ -61,24 +84,10 @@ function displayTVInfo() {
                 // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
                 $("#gifs-appear-here").prepend(gifDiv);
             }
-            //changes state of gif from still to animated or vice versa when gif is clicked
-            $(".gif").on("click", function () {
-                // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-                var state = $(this).attr("data-state");
-                // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-                // Then, set the image's data-state to animate
-                // Else set src to the data-still value
-                if (state === "still") {
-                    $(this).attr("src", $(this).attr("tvShow-animate"));
-                    $(this).attr("data-state", "animate");
-                } else {
-                    $(this).attr("src", $(this).attr("tvShow-still"));
-                    $(this).attr("data-state", "still");
-                }
-            });
+            singleClick ();
+            doubleClick();
+            
         });
-
-
 }
 
 // Function for displaying tv show data
@@ -121,9 +130,11 @@ $("#add-tv").on("click", function (event) {
 $(document).on("click", ".tvShow-btn", displayTVInfo);
 
 //clears local storage and favorite gifs held there
-$("#clear").on("click",function (){
+$("#clear").on("click", function () {
     localStorage.clear();
+    $("#favorite-gifs").empty();
 });
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
+$("#favorite-gifs").html(localStorage.getItem("favoriteGifs")||"");
